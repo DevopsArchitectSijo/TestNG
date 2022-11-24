d393736d6b533a304590e8c44fa278cb0f39baaf
DevopsArchitectSijo <53095503+DevopsArchitectSijo@users.noreply.github.com>
2022-11-24 13:15:33 +0530

    adding testng  sample project
    adding testng  sample project

---
+++ TestProject/src/com/bjs/test/TestBjsSite.java
@@ -0,0 +1,28 @@
+package com.bjs.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class TestBjsSite {
+	
+	@Test
+	public void openBjsSite() {
+			System.setProperty("webdriver.chrome.driver","C:\\Automation_Eclipse_WS\\TestProject\\BrowserDrivers\\chromedriver.exe");
+			WebDriver driver = new  ChromeDriver();
+		
+			System.out.println("------------------------------------------------------------------------------------------");
+			System.out.println("---------------------------------Validating BJ's logo-------------------------------------");
+			driver.get("https://www.bjs.com/");
+			System.out.println(driver.getTitle());
+			driver.findElement(By.xpath("//*[@id=\"header-margin\"]/ul/li[6]/div[1]/app-header-my-account/div/div/span/a")).click();
+			System.out.println(driver.getTitle());
+		
+				
+	}
+		
+	
+
+}
